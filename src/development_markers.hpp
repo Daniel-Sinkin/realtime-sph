6c182cdac677f890e0668463f3b425e1f319cd7b
Daniel-Sinkin <danielsinkin97@gmail.com>
2026-05-18 12:47:00 +0200

    Update ds-core v0.1.0 template markers

--- src/development_markers.hpp
+++ src/development_markers.hpp
@@ -15,6 +15,10 @@
 #    error "mut marker macro is already defined"
 #endif
 
+#if defined(mut_unchecked)
+#    error "mut_unchecked marker macro is already defined"
+#endif
+
 #if defined(cpy)
 #    error "cpy marker macro is already defined"
 #endif
@@ -25,12 +29,15 @@
 
 #if defined(__clang__)
 #    define mut [[clang::annotate("mut")]]
+#    define mut_unchecked [[clang::annotate("mut_unchecked")]]
 #    define cpy [[clang::annotate("cpy")]]
 #else
 #    define mut
+#    define mut_unchecked
 #    define cpy
 #endif
 
+// Only use this on the auto for functions when using trailing return
 #define def auto
 
 namespace realtime_sph
